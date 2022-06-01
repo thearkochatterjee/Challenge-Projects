fac276ddf7dfdaf1a4fae5dd9ecd83951510ef0c
Arko Chatterjee <arkochatterjee1006@gmail.com>
2022-05-31 20:19:40 -0400

    Getting all text from articles from yahoo result

--- Company Analysis/other_webscrapper.py
+++ Company Analysis/other_webscrapper.py
@@ -1,51 +1,35 @@
 import requests
 from bs4 import BeautifulSoup
-import webbrowser
 
 
-def getPage(company: str) -> requests.models.Response:
+def getPageGoogle(company: str) -> requests.models.Response:
     # webbrowser.open('https://google.com/search?q=' + company + '&tbm=nws')
     return requests.get('https://www.google.com/search?q=' + company + '&tbm=nws')
+
+
+def getPageYahoo(company: str) -> requests.models.Response:
+    return requests.get('https://news.search.yahoo.com/search?p=' + company + '&fr2=piv-web&fr=news&guccounter=1')
 
 
 def get_soup(page: requests.models.Response):
     return BeautifulSoup(page.content, 'lxml')
 
-company = 'apple'
-# response = requests.get('https://www.google.com/search?q=' + company + '&tbm=nws&start=0')
-response = requests.get('https://news.search.yahoo.com/search?p=' + company + '&fr2=piv-web&fr=news&guccounter=1')
-soup = BeautifulSoup(response.content, 'lxml')
-print(soup)
-# page = getPage('apple')
-# print(page.url)
-# soup = get_soup(page)
-# h = soup.find_all('h3')
-# for info in h:
-#     print(info.getText())
-#     print('\n\n\n')
-# print(soup.prettify())
-# body = soup.body
-# for i in list(body.children):
-#     print(i)
-#     print('\n\n\n')
 
-# for i in l:
-#     print(i)
-#     print('\n\n\n')
-# print(len(list(body.children)))
-# for link in body.find_all('a'):
-#     print(link.get('href'))
-# soup = get_soup(getPage('apple'))
-# full_text = soup.prettify()
-# full_text_arr = full_text.split('\n')
-# print(full_text_arr)
-# for i in range(0, len(full_text_arr)):
-#     if full_text_arr[i].strip().startswith('<'):
-#         print(full_text_arr[i])
-#     else:
-#         pass
-# print(soup.select('main'))
-# main = list(soup.find_all(id='main'))
-# main = list(main[0].children)
-# print(main[0].prettify())
-# print('hi')
+def getData(company: str) -> []:
+    soup = get_soup(getPageYahoo(company))
+    links = []
+    pages = []
+    page_texts = []
+    for link in soup.find_all('a'):
+        temp = link.get('href')
+        if 'yahoo.com' not in temp and len(temp) > 1:
+            links.append(temp)
+            pages.append(get_soup(requests.get(temp)))
+            t = pages[-1].findAll('p')
+            t2 = []
+            for i in t:
+                t2.append(i.text)
+            page_texts.append(''.join(t2))
+            print(page_texts[-1])
+    return links, page_texts
+
